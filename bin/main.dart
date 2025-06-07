d6b37aef6f3b2ff2eadb0da7c4aa6c41823d02e4
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-07 15:55:30 +0600

    first

---
+++ bin/main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main(){
+  var input = stdin.readLineSync()!;
+
+  if (input == "31231"){
+    print("Banana");
+  }
+  else if (input == "43861"){
+    print("Elderberry");
+  }
+  else if (input == "82678"){
+    print("Honeydew Melon");
+  }  else if (input == "23456"){
+    print("Apple");
+  }  else if (input == "78901"){
+    print("Mango");
+  }  else if (input == "98765"){
+    print("Nectarine");
+  }  else if (input == "67890"){
+    print("Raspberry");
+  }  else if (input == "21098"){
+    print("Tangerine");
+  }  else if (input == "45678"){
+    print("Orange");
+  }
+}
