98f5d44273ac178991db4cb436f3a46f41f8b257
Abdullah <abdullah.mamun8191@gmail.com>
2025-06-08 11:04:53 +0600

    Build Toy Car

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,34 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(){
-  var input = stdin.readLineSync()!;
 
-  if (input == "31231"){
-    print("Banana");
-  }
-  else if (input == "43861"){
-    print("Elderberry");
-  }
-  else if (input == "82678"){
-    print("Honeydew Melon");
-  }  else if (input == "23456"){
-    print("Apple");
-  }  else if (input == "78901"){
-    print("Mango");
-  }  else if (input == "98765"){
-    print("Nectarine");
-  }  else if (input == "67890"){
-    print("Raspberry");
-  }  else if (input == "21098"){
-    print("Tangerine");
-  }  else if (input == "45678"){
-    print("Orange");
-  }
+// wheel =43/4, bodies =15/1, Figure= 87/2
+  var input = stdin.readLineSync()!;
+ // print(input);
+
+  var data = input.split(" ");
+//  print(data);
+
+  int wheel = int.parse(data[0]);
+  int bodies = int.parse(data[1]) ;
+  int figure = int.parse(data[2]);
+/*
+  print(wheel);
+  print(bodies);
+  print(figure);*/
+
+  int wheelLimit = wheel~/4;  // ~ এই চিহ্ন ডার্ট এর ফ্লটিং পয়েন্ট নিরদেশ করে
+  int bodiesLimit = bodies~/1;
+  int figuresLimit = figure~/2;
+
+/*  print(wheelLimit);
+  print(bodiesLimit);
+  print(figuresLimit);*/
+
+  int carpossible = [wheelLimit, bodiesLimit, figuresLimit].reduce(min);
+  print("carpossible: $carpossible");
+
+
+
 }
